5bea48aa4c466470b2780af45e3184609c462728
woo3145 <lcwoo3145@gmail.com>
2023-04-07 11:31:04 +0900

    Solve - 13460(Simulation)

---
+++ Problems/Simulation/13460.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using std::pair;
+using std::vector;
+using std::queue;
+using std::ios;
+
+// https://www.acmicpc.net/problem/13460
+// 스타트링크에서 판매하는 어린이용 장난감 중에서 가장 인기가 많은 제품은 구슬 탈출이다. 
+// 구슬 탈출은 직사각형 보드에 빨간 구슬과 파란 구슬을 하나씩 넣은 다음, 빨간 구슬을 구멍을 통해 빼내는 게임이다.
+
+// 보드의 세로 크기는 N, 가로 크기는 M이고, 편의상 1×1크기의 칸으로 나누어져 있다. 
+// 가장 바깥 행과 열은 모두 막혀져 있고, 보드에는 구멍이 하나 있다. 빨간 구슬과 파란 구슬의 크기는 보드에서 1×1크기의 칸을 가득 채우는 사이즈이고, 각각 하나씩 들어가 있다.
+//  게임의 목표는 빨간 구슬을 구멍을 통해서 빼내는 것이다. 이때, 파란 구슬이 구멍에 들어가면 안 된다.
+
+// 이때, 구슬을 손으로 건드릴 수는 없고, 중력을 이용해서 이리 저리 굴려야 한다. 
+// 왼쪽으로 기울이기, 오른쪽으로 기울이기, 위쪽으로 기울이기, 아래쪽으로 기울이기와 같은 네 가지 동작이 가능하다.
+
+// 각각의 동작에서 공은 동시에 움직인다. 빨간 구슬이 구멍에 빠지면 성공이지만, 파란 구슬이 구멍에 빠지면 실패이다. 
+// 빨간 구슬과 파란 구슬이 동시에 구멍에 빠져도 실패이다. 빨간 구슬과 파란 구슬은 동시에 같은 칸에 있을 수 없다. 또, 빨간 구슬과 파란 구슬의 크기는 한 칸을 모두 차지한다.
+//  기울이는 동작을 그만하는 것은 더 이상 구슬이 움직이지 않을 때 까지이다.
+
+// 보드의 상태가 주어졌을 때, 최소 몇 번 만에 빨간 구슬을 구멍을 통해 빼낼 수 있는지 구하는 프로그램을 작성하시오.
+
+struct Position {
+    int y;
+    int x;
+    Position(){};
+    Position(int _y, int _x): y(_y), x(_x){};
+};
+
+int N, M;
+vector<vector<char>> board;
+Position red;
+Position blue;
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+
+int count;
+
+void Input() {
+    std::cin >> N >> M;
+    board.assign(N,vector<char>(M));
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < M; ++j){
+            std::cin >> board[i][j];
+            if(board[i][j] == 'R') {red.y = i; red.x = j;}
+            if(board[i][j] == 'B') {blue.y = i; blue.x = j;}
+        }
+    }
+}
+// 구슬을 이동시키는 함수
+// 이동 후 빨간구슬과 파란구슬이 겹치는지 확인 필요
+// 이동 후 구슬이 들어가는지 확인 필요
+pair<Position, Position> Move(const Position& r, const Position& b, int direction) {
+    int rY = r.y;
+    int rX = r.x;
+    int bY = b.y;
+    int bX = b.x;
+    bool crossR = false;
+    bool crossB = false;
+
+    while(board[rY+dy[direction]][rX+dx[direction]] != '#') {
+        rY += dy[direction];
+        rX += dx[direction];
+        if(rY == bY && rX == bX) crossB = true;
+        if(board[rY][rX] == 'O') break;
+    }
+    while(board[bY+dy[direction]][bX+dx[direction]] != '#') {
+        bY += dy[direction];
+        bX += dx[direction];
+        if(rY == bY && rX == bX && !crossB) crossR = true;
+        if(board[bY][bX] == 'O') break;
+    }
+
+    if((rY == bY && rX == bX && board[rY][rX] != 'O')){
+        if(crossB) {
+            rY -= dy[direction];
+            rX -= dx[direction];
+        }
+        if(crossR) {
+            bY -= dy[direction];
+            bX -= dx[direction];
+        }
+    }
+    return {Position(rY, rX), Position(bY, bX)};
+}
+
+bool Bfs(const Position& r, const Position& b) {
+    queue<pair<pair<Position, Position>, int>> que;
+    que.push({{r,b}, 0});
+
+    while(!que.empty()){
+        Position curRed = que.front().first.first;
+        Position curBlue = que.front().first.second;
+        int cnt = que.front().second;
+        que.pop();
+
+        // 문제조건: 10번 이하로 움직여서 구슬을 못빼면 -1
+        if(10 < cnt) {
+            count = -1;
+            break;
+        }
+        // 파란구슬이 빠진경우 이후 경로 진행x
+        if(board[curBlue.y][curBlue.x] == 'O') {
+            continue;
+        };
+        // 최소경로 발견
+        if(board[curRed.y][curRed.x] == 'O') {
+            count = cnt;
+            return true;
+        }
+
+        for(int i = 0; i < 4; ++i){
+            pair<Position, Position> nextPos = Move(curRed, curBlue, i);
+            if(nextPos.first.y != curRed.y || nextPos.first.x != curRed.x 
+            || nextPos.second.y != curBlue.y || nextPos.second.x != curBlue.x) {
+                que.push({nextPos, cnt + 1});
+            }
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    Input();
+    bool success = Bfs(red, blue);
+
+    if(success) {
+        std::cout << count << '\n';
+    }else {
+        std::cout << -1 << '\n';
+    }
+    
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,36 +1,178 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
-#define MOD 1000000007
-
+using std::pair;
 using std::vector;
+using std::queue;
 using std::ios;
 
-int N;
-vector<long long> dp;
-vector<long long> dpSum;
+struct Position {
+    int y;
+    int x;
+    Position(){};
+    Position(int _y, int _x): y(_y), x(_x){};
+};
+
+int N, M;
+vector<vector<char>> board;
+Position red;
+Position blue;
+int count;
 
 void Input() {
-    std::cin >> N;
-    dp.assign(N+1, 0);
-    dpSum.assign(N+1, 0);
+    std::cin >> N >> M;
+    board.assign(N,vector<char>(M));
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < M; ++j){
+            std::cin >> board[i][j];
+            if(board[i][j] == 'R') {red.y = i; red.x = j;}
+            if(board[i][j] == 'B') {blue.y = i; blue.x = j;}
+        }
+    }
+}
+
+pair<Position, Position> Left( const Position& r, const Position& b) {
+    int rX = r.x;
+    int bX = b.x;
+
+    bool crossR = false;
+    bool crossB = false;
+
+    while(0 < rX && board[r.y][rX-1] != '#') {
+        --rX;
+        if(r.y == b.y && rX == bX) crossB = true;
+        if(board[r.y][rX] == 'O') break;
+    }
+    while(0 < bX && board[b.y][bX-1] != '#') {
+        --bX;
+        if(r.y == b.y && rX == bX && !crossB) crossR = true;
+        if(board[b.y][bX] == 'O') break;
+    }
+    if((r.y == b.y && rX == bX && board[r.y][rX] != 'O')){
+        if(crossB) rX += 1;
+        if(crossR) bX += 1;
+    }
+    return {Position(r.y, rX), Position(b.y, bX)};
+}
+pair<Position, Position> Right(const Position& r, const Position& b) {
+    int rX = r.x;
+    int bX = b.x;
+
+    bool crossR = false;
+    bool crossB = false;
+
+    while(rX < M && board[r.y][rX+1] != '#') {
+        ++rX;
+        if(r.y == b.y && rX == bX) crossB = true;
+        if(board[r.y][rX] == 'O') break;
+    }
+    while(bX < M && board[b.y][bX+1] != '#') {
+        ++bX;
+        if(r.y == b.y && rX == bX && !crossB) crossR = true;
+        if(board[b.y][bX] == 'O') break;
+    }
+    if((r.y == b.y && rX == bX && board[r.y][rX] != 'O')){
+        if(crossB) rX -= 1;
+        if(crossR) bX -= 1;
+    }
+    return {Position(r.y, rX), Position(b.y, bX)};
+} 
+pair<Position, Position> Up(const Position& r, const Position& b) {
+    int rY = r.y;
+    int bY = b.y;
+
+    bool crossR = false;
+    bool crossB = false;
+
+    while(0 < rY && board[rY-1][r.x] != '#') {
+        --rY;
+        if(rY == bY && r.x == b.x) crossB = true;
+        if(board[rY][r.x] == 'O') break;
+    }
+    while(0 < bY && board[bY-1][b.x] != '#') {
+        --bY;
+        if(rY == bY && r.x == b.x && !crossB) crossR = true;
+        if(board[bY][b.x] == 'O') break;
+    }
+    if((rY == bY && r.x == b.x && board[rY][r.x] != 'O')){
+        if(crossB) rY += 1;
+        if(crossR) bY += 1;
+    }
+    return {Position(rY, r.x), Position(bY, b.x)};
+}
+pair<Position, Position> Down(const Position& r, const Position& b) {
+    int rY = r.y;
+    int bY = b.y;
+
+    bool crossR = false;
+    bool crossB = false;
+
+    while(rY < N && board[rY+1][r.x] != '#') {
+        ++rY;
+        if(rY == bY && r.x == b.x) crossB = true;
+        if(board[rY][r.x] == 'O') break;
+    }
+    while(bY < N && board[bY+1][b.x] != '#') {
+        ++bY;
+        if(rY == bY && r.x == b.x && !crossB) crossR = true;
+        if(board[bY][b.x] == 'O') break;
+    }
+    if((rY == bY && r.x == b.x && board[rY][r.x] != 'O')){
+        if(crossB) rY -= 1;
+        if(crossR) bY -= 1;
+    }
+    return {Position(rY, r.x), Position(bY, b.x)};
+}
+
+bool Bfs(const Position& r, const Position& b) {
+    queue<pair<pair<Position, Position>, int>> que;
+    que.push({{r,b}, 0});
+
+    while(!que.empty()){
+        Position curRed = que.front().first.first;
+        Position curBlue = que.front().first.second;
+        int cnt = que.front().second;
+        que.pop();
+
+        if(10 < cnt) {
+            count = -1;
+            break;
+        }
+        if(board[curBlue.y][curBlue.x] == 'O') {
+            continue;
+        };
+        if(board[curRed.y][curRed.x] == 'O') {
+            count = cnt;
+            return true;
+        }
+
+        if(0 < curBlue.y && board[curBlue.y-1][curBlue.x] != '#' || 0 < curRed.y && board[curRed.y-1][curRed.x] != '#'){
+            que.push({Up(curRed, curBlue), cnt + 1});
+        }
+        if(curBlue.y < N && board[curBlue.y+1][curBlue.x] != '#' || curRed.y < N && board[curRed.y+1][curRed.x] != '#'){
+            que.push({Down(curRed, curBlue), cnt + 1});
+        }
+        if(0 < curBlue.x && board[curBlue.y][curBlue.x-1] != '#' || 0 < curRed.x && board[curRed.y][curRed.x-1] != '#'){
+            que.push({Left(curRed, curBlue), cnt + 1});
+        }
+        if(curBlue.x < M && board[curBlue.y][curBlue.x+1] != '#' || curRed.x < M && board[curRed.y][curRed.x+1] != '#'){
+            que.push({Right(curRed, curBlue), cnt + 1});
+        }
+    }
+
+    return false;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-    
     Input();
-    dp[1] = 2;
-    dp[2] = 7;
-    dpSum[2] = 1; // dpSum[3]부터 2가지 방법이 추가됨으로 임시값
+    bool success = Bfs(red, blue);
 
-    for(int i = 3; i <= N; ++i){
-        dpSum[i] = (dp[i-3] + dpSum[i-1]) % MOD;
-        dp[i] = (2 * dp[i-1] + 3 * dp[i-2] + 2 * dpSum[i]) % MOD;
+    if(success) {
+        std::cout << count << '\n';
+    }else {
+        std::cout << -1 << '\n';
     }
-    std::cout << dp[N] << '\n';
-
+    
     return 0;
 }
