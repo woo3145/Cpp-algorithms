321dd53b243087a76c8003371b0353673f6d61ee
woo3145 <lcwoo3145@gmail.com>
2023-06-24 20:18:11 +0900

    Solve - 2293(DP)

---
+++ Problems/DP/2293.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::ios;
+
+// https://www.acmicpc.net/problem/2293
+// 동전 1
+
+// n가지 종류의 동전으로 목표가치 K를 만드는 경우의 수를 구하는 문제
+// 사용한 동전의 종류는 같고, 순서만 다른 경우 같은 경우로 침
+
+int N, K;
+int coin[100];
+int dp[10001];
+
+void Input() {
+    cin >> N >> K;
+    for(int i = 0; i < N; ++i){
+        cin >> coin[i];
+    }
+}
+
+void Solve() {
+    dp[0] = 1;
+    for(int c = 0; c < N; ++c){
+        int value = coin[c];
+
+        int cur = value;
+        while(cur <= K){
+            dp[cur] += dp[cur - value];
+            ++cur;
+        }
+    }
+
+    cout << dp[K] << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    Input();
+    Solve();
+    return 0;
+}
