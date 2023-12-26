99c57b6bc3a4b61a130a0d944e0bd703039cc0a3
woo3145 <lcwoo3145@gmail.com>
2023-12-27 01:05:47 +0900

    설명 추가 - miller_rabin.cpp

--- Number/miller_rabin.cpp
+++ Number/miller_rabin.cpp
@@ -1,59 +1,88 @@
 #include <vector>
 #include <iostream>
 
+#define ull unsigned long long
+
 using std::vector;
 using std::cout;
 using std::cin;
 
-long long modPow (long long base, long long exp, long long mod) {
-    long long ret = 1;
-    while(exp){
-        if(exp & 1) ret = (ret * base) % mod;
-        base = (base*base) % mod;
-        exp >>= 1;
+
+// 페르마의 소정리
+// 1. 소수 p와 정수 a에 대해 다음을 만족함 (a는 p의 배수가 아님)
+
+// a^p(mod p) = a(mod p)
+// a^(p-1)(mod p) = 1
+
+// 2. 소수 p에 대해 x^2(mod p) = 1이라면, x(mod p) = -1 or x(mod p) = 1 이다.
+
+
+// 밀러-라빈 소수 판별
+
+// N이 소수일때 N-1은 짝수가 된다.
+// 따라서 다음과 같이 정리 가능 :  N-1 = d * 2^r  (d는 홀수)
+
+// N이 소수라면 정수 a에 대해 페르마의 소정리를 만족하게 된다.
+// a^(N-1)(mod N) = 1, a^(d*2^r)(mod N) = 1
+
+// 따라서 d = N-1 부터 2로 나누며 거듭제곱 모듈러 연산을 한다.
+// d가 짝수라면 나머지가 -1일때 소수일 가능성이 존재한다.
+    // 나머지가 1이라면 위의 페르마 소정리 2번에 따라 a^(d*2^(r-1))(mod N)가 -1 or 1일 수 있기 때문에 판단보류
+// d가 홀수라면(마지막) 나머지가 -1 또는 1이 나오면 소수일 가능성이 존재한다.
+
+
+ull modPow(ull base, ull p, ull mod) {
+    ull ret = 1;
+    base = base % mod;
+    while(p) {
+        if(p & 1) ret = (ret * base) % mod;
+        base = (base * base) % mod;
+        p >>= 1;
     }
     return ret;
 }
 
-bool miller(long long num, long long seed){
-    if(num % seed == 0) return num == seed;
+// true: 합성수
+// false: 소수일 가능성
+bool miller_rabin(ull num, ull seed) {
+    if (num % seed == 0) return true;
 
-    long long d = num - 1;
-    while (d % 2 == 0) d /= 2;
+    ull d = num - 1;
+    while (1) {
+		ull tmp = modPow(seed, d, num);
+		if (d & 1)
+			return (tmp != 1 && tmp != num - 1); // d가 1도 안되고 -1도 안되면 합성수 (false)
+		else if (tmp == num - 1) // a^(d*2^r)(mod N)이 -1이라면 소수일 가능성 (결과가 1이면 페르마 정리 2번에 따라 다시 나눠짐)
+                                // (나머지가 1이라면 위의 페르마 소정리 2번에 따라 a^(d*2^(r-1))(mod N)가 -1 or 1일 수 있기 때문에 판단보류)
+			return 0;
+		d >>= 1;
 
-    long long tmp = modPow(seed, d, num);
-    if (tmp == 1 || tmp == num - 1) return false;
+       
+	}
 
-    while (d != num - 1) {
-        tmp = (tmp * tmp) % num;
-        d *= 2;
-
-        if (tmp == 1) return true;
-        if (tmp == num - 1) return false;
-    }
 
     return true;
 }
 
+bool isPrime(ull n) {
+	if (n < 2 || (n % 2 == 0))
+		return (n == 2);
 
-bool isPrime(long long num){
-    if(num < 2 || num % 2 == 0){
-        return num == 2;
-    }
-
-    vector<long long> seeds = {2, 3, 5, 7, 11, 13};
-
-    for(const auto& seed : seeds){
-        if(num == seed) return true;
-
-        if(miller(num, seed)) return false;
-    }  
-
-    return true;
+	vector<ull> seeds({2, 7, 61});
+	
+	for(auto& seed : seeds) {
+		if (n == seed)
+			return 1;
+		if (miller_rabin(n, seed))
+			return 0;
+	}
+	return 1;
 }
 
-int main() {
-    long long a = 2305843009213693951;
-
-    cout << isPrime(a);
+int main () {
+    for(int i = 0; i < 1000; ++i){
+        if(isPrime(i)) {
+            cout << i << '\n';
+        }
+    }
 }
