ab7a3c13264d92f9e968ea53691d017831853e66
woo3145 <lcwoo3145@gmail.com>
2023-12-28 11:52:56 +0900

    kmp, matrix약간 수정

--- String/kmp.cpp
+++ String/kmp.cpp
@@ -9,9 +9,9 @@ using std::string;
 // lps[i]: 0 ~ i까지의 문자열에서 접미사면서 가장긴 접두사의 길이를 저장
 // "abcabc" -> abc , "a"bc"a" , "ab"c"ab" , "abc""abc"
 //  000123      0        1          2           3
-vector<int> CreateLPS(const string& pattern) {
+void CreateLPS(vector<int>& ret, const string& pattern) {
     int patternLen = pattern.length();
-    vector<int> lps(patternLen, 0);
+    ret.assign(patternLen, 0);
 
     int prefix = 0; // 접두사 길이
 
@@ -19,16 +19,15 @@ vector<int> CreateLPS(const string& pattern) {
     for(int i = 1; i < patternLen; ++i) {
         // 현재 prefix 마지막 문자열이 일치하지 않을 경우 prefix = lps[prefix-1] (prefix가 0이거나 문자열이 같을때까지)
         while(prefix > 0 && pattern[prefix] != pattern[i]) {
-            prefix = lps[prefix - 1];
+            prefix = ret[prefix - 1];
         }
         // 일치하면 prefix 길이 증가
         if(pattern[prefix] == pattern[i]){
             prefix += 1;
         }
         // lps 배열 갱신
-        lps[i] = prefix;
+        ret[i] = prefix;
     }
-    return lps;
 }
 
 // KMP: 문자열의 비교는 O(n)으로 작동함
@@ -36,7 +35,8 @@ vector<int> CreateLPS(const string& pattern) {
 void KMP(const string& str, const string& pattern) {
     int strLen = str.length();
     int patternLen = pattern.length();
-    vector<int> lps = CreateLPS(pattern);
+    vector<int> lps;
+    CreateLPS(lps, pattern);
 
     int j = 0; // 탐색중인 pattern의 인덱스
 
